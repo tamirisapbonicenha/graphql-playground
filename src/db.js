1a4e59b07d7a6ade086c7c0cc70ff49a0cced234
Tamiris Bonicenha <tamirisapbonicenha@gmail.com>
2021-02-27 22:13:58 -0300

    feat: starting to play

---
+++ src/db.js
@@ -0,0 +1,5 @@
+export let users = [
+  { id: 1, name: "Tamiris", email: "tamiris@gmail.com", age: 30 },
+  { id: 2, name: "João", email: "joao@gmail.com", age: 28 },
+  { id: 3, name: "Paulo Rick", email: "paulo@gmail.com", age: 24 },
+];
